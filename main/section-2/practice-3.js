92615d61bbe6e1cf64c35f71fedb4f84a8b836fd
2228787638 <2228787638@qq.com>
2019-07-13 19:34:28 +0800

    feat:implements section-2 pratice-3

--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,5 +1,35 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  const collectionSet = new Set();
+  //过滤拆分字符串
+  collection.forEach(item => {
+    if(item.indexOf('-')||item.indexOf(':')||item.indexOf('[')){
+      collectionSet.add(item.split(/-|:|\[/)[0]);
+    }else{
+      collectionSet.add(item);
+    }
+  });
+  collection.forEach((value,key,arr) => {
+    if(value.indexOf('-')!=-1||value.indexOf(':')!=-1||value.indexOf('[')!=-1){
+      for(let i = 0; i < parseInt(value.split(/-|:|\[/)[1]); i++){
+        collection.push(value.split(/-|:|\[/)[0]);
+      }
+      collection.splice(key, 1);
+    }
+  });
+  const object = {};
+  collection.forEach(element => {
+    if(object[element]) object[element]++;
+    else object[element] = 1;
+  })
+  const collectionResult = new Array();
+  collectionSet.forEach(element => {
+    for(let item in object){
+      if(element === item){
+        collectionResult.push({name: item, summary: object[item]})
+      }
+    }
+  })
+  return collectionResult;
 }
