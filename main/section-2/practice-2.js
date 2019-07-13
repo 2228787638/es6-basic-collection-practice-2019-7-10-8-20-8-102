05c14b8640fe7e40bd049bb136aea298b94bdac9
2228787638 <2228787638@qq.com>
2019-07-13 17:47:20 +0800

    feat:implements section-2 pratice-2

--- main/section-2/practice-2.js
+++ main/section-2/practice-2.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  const resultCollection = new Array();
+  const resultMap = new Map();
+  collection.forEach(item => {
+    resultMap.set(item, resultMap.get(item) + 1||1);
+  });
+  resultMap.forEach((value, key, resultMap) => {
+    if(key.indexOf('-')===-1){
+      resultCollection.push( {key: key, count: value} );
+    }else{
+      resultCollection.push( { key: key.split('-')[0],count: parseInt(key.split('-')[1])} )
+    }
+  });
+  return resultCollection;
 }
